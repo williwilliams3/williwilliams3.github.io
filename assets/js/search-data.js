20882297ef092987154e09c840683af2d0fe360e
williwilliams3 <47433825+williwilliams3@users.noreply.github.com>
2025-12-12 04:35:34 +0000

    Deploying to gh-pages from @ williwilliams3/williwilliams3.github.io@eed5fab0b19a6d45eda30a329b1718d90cdca4e6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-",
-          title: "",
-          description: "Download my CV by clicking on the icon.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
